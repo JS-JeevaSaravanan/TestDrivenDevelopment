d855dd9798d6f457858ad62452bd8918c7325731
JS_JeevaSaravanan <jeevatamil2000@gmail.com>
2024-12-06 18:06:47 +0530

    ✅ pass(test): two numbers with comma in StringCalculator.Add

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,6 +1,14 @@
 export class StringCalculator {
-  public Add(numbers: string): number {
-    if (!numbers) return 0;
-    return parseInt(numbers);
+  public Add(numbersString: string): number {
+    if (!numbersString) {
+      return 0;
+    }
+    if (!numbersString.includes(",")) {
+      return parseInt(numbersString);
+    }
+    return (
+      parseInt(numbersString.split(",")[0]) +
+      parseInt(numbersString.split(",")[1])
+    );
   }
 }
