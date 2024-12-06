43b0d9eb941af0c284cbcbff47a3e26d6c417f98
JS_JeevaSaravanan <jeevatamil2000@gmail.com>
2024-12-06 18:58:09 +0530

    ✅ pass(test): support custom  delimiter in StringCalculator.Add

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,13 +1,31 @@
 export class StringCalculator {
-  public Add(numbersString: string): number {
-    if (!numbersString) {
+  private getNumbersToSum(numbersStr: string): string[] {
+    const customDelimiterKeyword = "//";
+    if (numbersStr.startsWith(customDelimiterKeyword)) {
+      const delimiterEndIndex = numbersStr.indexOf("\n");
+      const delimiter = numbersStr.slice(
+        customDelimiterKeyword.length,
+        delimiterEndIndex
+      );
+      const numbers = numbersStr.slice(delimiterEndIndex + 1);
+      return this.splitNumbers(numbers, delimiter);
+    }
+    return this.splitNumbers(numbersStr);
+  }
+
+  private splitNumbers(numbersStr: string, delimiter: string = ","): string[] {
+    const delimiterRegex = new RegExp(`[${delimiter}\n]`);
+    return numbersStr.split(delimiterRegex).map((num) => num.trim());
+  }
+
+  public Add(numbersStr: string): number {
+    if (!numbersStr) {
       return 0;
     }
 
-    const numArray = numbersString
-      .replace(/\n/g, ",")
-      .split(",")
-      .map((num) => parseInt(num.trim()));
-    return numArray.reduce((sum, num) => sum + num, 0);
+    const numberStrToSum = this.getNumbersToSum(numbersStr);
+    return numberStrToSum
+      .map((num) => parseInt(num))
+      .reduce((sum, num) => sum + num, 0);
   }
 }
