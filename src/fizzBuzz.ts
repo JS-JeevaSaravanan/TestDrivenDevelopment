18cf68fc3f40ab424dfd2e7a5f6fc3b0e7beb8eb
JS_JeevaSaravanan <jeevatamil2000@gmail.com>
2025-01-08 09:27:45 +0530

    ✅ pass(test): Buzz for 10

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,6 +1,6 @@
 const fizzBuzz = (num: number): string => {
   if (num % 3 === 0) return "Fizz";
-  else if (num === 5) return "Buzz";
+  else if (num === 5 || num === 10) return "Buzz";
   return num.toString();
 };
 
