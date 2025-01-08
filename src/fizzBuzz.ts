c1e53d7ba243d025b5eb3014d468b23c6ac610ff
JS_JeevaSaravanan <jeevatamil2000@gmail.com>
2025-01-08 09:38:57 +0530

    ✅  pass(test): FizzBuzz for 15

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,5 +1,7 @@
 const fizzBuzz = (num: number): string => {
   switch (true) {
+    case num === 15:
+      return "FizzBuzz";
     case num % 3 === 0:
       return "Fizz";
     case num % 5 === 0:
