2b89de7149e483af08e62e8abccbb22e9ee4576c
JS_JeevaSaravanan <jeevatamil2000@gmail.com>
2025-01-08 09:22:20 +0530

    ✨ add(tests): few extra test cases added + test flow refactored

--- tests/fizzBuzz.test.ts
+++ tests/fizzBuzz.test.ts
@@ -1,17 +1,21 @@
 import { fizzBuzz } from "../src/fizzBuzz";
 
 describe("Fizz Buzz", () => {
-  test.each([
-    [1, "1"],
-    [2, "2"],
-    [4, "4"],
-  ])("Should return %s for %i", (input, expectedOutput) => {
-    const result = fizzBuzz(input);
-    expect(result).toBe(expectedOutput);
+  describe("Numbers that are not divisible by 3 or 5", () => {
+    test.each([
+      [1, "1"],
+      [2, "2"],
+      [4, "4"],
+    ])("Should return %s for %i", (input, expectedOutput) => {
+      const result = fizzBuzz(input);
+      expect(result).toBe(expectedOutput);
+    });
   });
 
-  test.each([[3], [6]])("Should return Fizz for %i", (input) => {
-    const result = fizzBuzz(input);
-    expect(result).toBe("Fizz");
+  describe("Numbers divisible by 3", () => {
+    test.each([[3], [6], [9], [1230]])("Should return Fizz for %i", (input) => {
+      const result = fizzBuzz(input);
+      expect(result).toBe("Fizz");
+    });
   });
 });
