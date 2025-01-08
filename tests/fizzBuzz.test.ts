e21bfa5c6e702da7862e6a53938bb61fd935c36e
JS_JeevaSaravanan <jeevatamil2000@gmail.com>
2025-01-08 08:50:59 +0530

    🧪 add(test): 4 give 4

--- tests/fizzBuzz.test.ts
+++ tests/fizzBuzz.test.ts
@@ -5,8 +5,14 @@ describe("Fizz Buzz", () => {
     const result = fizzBuzz(1);
     expect(result).toBe("1");
   });
+
   it("Should return 2 for 2", () => {
     const result = fizzBuzz(2);
     expect(result).toBe("2");
+  });
+
+  it("Should return 4 for 4", () => {
+    const result = fizzBuzz(4);
+    expect(result).toBe("4");
   });
 });
