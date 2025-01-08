99410b97134b7a0741af785afe4c529d6ac589ee
JS_JeevaSaravanan <jeevatamil2000@gmail.com>
2025-01-08 10:38:30 +0530

    🧪 add(test): primeFactors of 1 is empty list

--- tests/primeFactors.test.ts
+++ tests/primeFactors.test.ts
@@ -1,3 +1,8 @@
 import { primeFactors } from "../src/primeFactors";
 
-describe("Prime Factors", () => {});
+describe("Prime Factors", () => {
+  it("Prime factors of 1 should be an empty array", () => {
+    const result = primeFactors(1);
+    expect(result).toEqual([]);
+  });
+});
