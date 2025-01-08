ac8cdda658d8fb5cda7e00a8a764058cf4008451
JS_JeevaSaravanan <jeevatamil2000@gmail.com>
2025-01-08 11:13:22 +0530

    ✅ pass(test): pFactors of 25 is [5,5]

--- src/primeFactors.ts
+++ src/primeFactors.ts
@@ -2,22 +2,12 @@ const primeFactors = (num: number): number[] => {
   const factorList: number[] = [];
   let remainder = num;
 
-  if (remainder > 1) {
-    while (remainder % 2 === 0) {
-      factorList.push(2);
-      remainder = remainder / 2;
-    }
-
-    while (remainder % 3 === 0) {
-      factorList.push(3);
-      remainder = remainder / 3;
-    }
-
-    if (remainder > 1) {
-      factorList.push(remainder);
+  for (let factor = 2; factor <= remainder; factor++) {
+    while (remainder % factor === 0) {
+      factorList.push(factor);
+      remainder /= factor;
     }
   }
-
   return factorList;
 };
 
