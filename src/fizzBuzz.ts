34f5ac65a8d69efeacdbd59fc8577201b90367fc
JS_JeevaSaravanan <jeevatamil2000@gmail.com>
2025-01-08 08:36:41 +0530

    🧪add(test): FizzBuzz - init case - 1 return 1

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,3 +1,3 @@
-const fizzBuzz = () => {};
+const fizzBuzz = (num: Number) => {};
 
 export { fizzBuzz };
--- tests/fizzBuzz.test.ts
+++ tests/fizzBuzz.test.ts
@@ -1,3 +1,8 @@
 import { fizzBuzz } from "../src/fizzBuzz";
 
-describe("Fizz Buzz", () => {});
+describe("Fizz Buzz", () => {
+  it("Should return 1 for 1", () => {
+    const result = fizzBuzz(1);
+    expect(result).toBe("1");
+  });
+});
