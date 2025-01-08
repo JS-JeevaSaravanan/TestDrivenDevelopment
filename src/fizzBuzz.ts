2b4084a49af9f1dc75a82623fd56a7889b243799
JS_JeevaSaravanan <jeevatamil2000@gmail.com>
2025-01-08 09:36:39 +0530

    ⏪️ revert(logic): futuristic logic reverted

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,7 +1,5 @@
 const fizzBuzz = (num: number): string => {
   switch (true) {
-    case num % 3 === 0 && num % 5 === 0:
-      return "FizzBuzz";
     case num % 3 === 0:
       return "Fizz";
     case num % 5 === 0:
