e25e5f0da3af17fbee79264b1037a9384fc12c42
JS-JeevaSaravanan <jeevatamil2000@gmail.com>
2025-02-03 18:48:30 +0530

    feat: should return false if year is divisible by 4, divisible by 100 but not divisible by 400

--- tests/leapYear.test.ts
+++ tests/leapYear.test.ts
@@ -14,5 +14,9 @@ describe("leap year test", () => {
         expect(isLeapYear(1600)).toBe(true);
     })
 
+    it('should return false if year is divisible by 4, divisible by 100 but not divisible by 400', () => {
+        expect(isLeapYear(1800)).toBe(false);
+    })
+
 })
 
