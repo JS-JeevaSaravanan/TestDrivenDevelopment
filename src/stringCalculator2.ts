7a1e148a664dd2ef9e2c67ccaa7cb7b33de22217
JS-JeevaSaravanan <jeevatamil2000@gmail.com>
2025-02-05 10:24:26 +0530

    string calc

--- src/stringCalculator2.ts
+++ src/stringCalculator2.ts
@@ -1,9 +1,43 @@
 
-export function  stringCalculator2(input:string) {
-    if(input.length === 0){
+export function stringCalculator2(input: string) {
+    if (input.length === 0) {
         return 0;
-    }else{
-        const numbers = input.replace(/\n/g, ",").split(",");
-        return numbers.reduce((acc, num) => acc + parseInt(num), 0);
+    } else {
+        const delimiters = [",", "\n"]
+
+        if (input.startsWith("//")) {
+            if (input.startsWith("//[")) {
+                const delimitersStr = input.slice(
+                    2,
+                    input.indexOf("\n")
+                );
+                const delimitersRegex = new RegExp(`\\${"["}(.*?)\\${"]"}`, "g");
+                const allDelimiterMatches = [
+                    ...delimitersStr.matchAll(delimitersRegex),
+                ];
+                allDelimiterMatches.forEach((match) => {
+                    delimiters.push(match[1]);
+                });
+            } else {
+                const splitter = input.substring(2, 3);
+                delimiters.push(splitter)
+                input = input.slice(3)
+            }
+        }
+        const delimiterRegex = new RegExp(`[${delimiters.join("")}]`);
+        const numbers = input
+            .split(delimiterRegex)
+        const negatives = numbers.find(n => parseInt(n) < 0);
+        if (negatives) throw new Error('negatives')
+
+
+        let sumValue = 0;
+        numbers.forEach(num => {
+            const numVal = parseInt(num);
+            if (numVal < 1001) {
+                sumValue += numVal
+            }
+        })
+        return sumValue;
     }
 }
--- tests/stringCalculator2.test.ts
+++ tests/stringCalculator2.test.ts
@@ -1,4 +1,4 @@
-import { stringCalculator2} from "../src/stringCalculator2";
+import { stringCalculator2 } from "../src/stringCalculator2";
 
 describe("string calculator", () => {
     it('should return 0 when empty string', () => {
@@ -16,4 +16,29 @@ describe("string calculator", () => {
     it('should return sum of numbers when input string numbers are split with newLine or comma', () => {
         expect(stringCalculator2("1\n2,3")).toBe(6);
     });
+
+    it("Adds two numbers with a single character delimiter", () => {
+        expect(stringCalculator2("//;\n1;2")).toBe(3);
+    });
+
+    it("Throws an error for negative numbers", () => {
+        expect(() => stringCalculator2("1,-2,-3")).toThrow('negatives');
+    });
+
+    it("Ignores numbers greater than 1000", () => {
+        expect(stringCalculator2("1001, 2")).toBe(2);
+    });
+
+    it("Adds numbers with a multi-character delimiter", () => {
+        expect(stringCalculator2("//[***]\n1***2***3")).toBe(6);
+    });
+
+    it("Adds numbers with multiple delimiters", () => {
+        expect(stringCalculator2("//[*][%]\n1*2%3")).toBe(6);
+    });
+
+    it("Adds numbers with multiple multi-character delimiters", () => {
+        expect(stringCalculator2("//[foo][bar]\n1foo2bar3")).toBe(6);
+    });
+
 })
